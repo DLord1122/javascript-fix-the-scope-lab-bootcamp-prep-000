74696b518cae85a215d6fe1d99ab43548966d750
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 16:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,5 @@ var funkyFunction = function() {return function() {return "FUNKY!"
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction() 
-
-theFunk()
+var theFunk = funkyFunction()()
 
